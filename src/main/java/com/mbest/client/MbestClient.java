aa788050205e09ce420d13b0694421e84880cf43
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-06 01:14:19 +0300

    Create MbestClient.java

---
+++ src/main/java/com/mbest/client/MbestClient.java
@@ -0,0 +1,10 @@
+package com.mbest.client;
+import net.fabricmc.api.ClientModInitializer;
+
+public class MbestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Mbest Client 1.20.2 | Baslatiliyor...");
+    }
+}
+
