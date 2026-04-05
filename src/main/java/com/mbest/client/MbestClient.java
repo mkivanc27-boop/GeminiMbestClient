33cf0e00787760c75c267f76093c5f367a1e4af3
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-06 02:27:07 +0300

    Update MbestClient.java

--- src/main/java/com/mbest/client/MbestClient.java
+++ src/main/java/com/mbest/client/MbestClient.java
@@ -1,10 +1,20 @@
 package com.mbest.client;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class MbestClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        System.out.println("Mbest Client 1.20.2 | Baslatiliyor...");
+        // ANDROID 12 DOUBLE/INTEGER BUG FIX
+        // Bu kod, sistemdeki hatalı ölçeklendirme değerini zorla Integer yapar
+        try {
+            String existingScale = System.getProperty("sun.java2d.uiScale");
+            if (existingScale != null) {
+                double scale = Double.parseDouble(existingScale);
+                System.setProperty("sun.java2d.uiScale", String.valueOf((int) scale));
+            }
+        } catch (Exception ignored) {}
+
+        System.out.println("Mbest Client Başlatıldı! Sürüm: 1.20.2");
     }
 }
-
